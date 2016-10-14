d79f5c72198f2a05edba8440b99adff85d6b125b
Daniel Sjösten <dansjo_87@hotmail.com>
2016-10-14 15:05:52 +0200

    Changes in dataanalyst

--- src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
@@ -27,6 +27,8 @@ public class DataAnalysisTest {
 		DataAnalysis dataAnalysis = new DataAnalysis();
 		ArrayList<String> values = new ArrayList<String>();
 		ArrayList<String> values1 = new ArrayList<String>();
+		ArrayList<String> values2 = new ArrayList<String>();
+	
 		
 		values.addAll(Arrays.asList("Game", "123", "123", "M", "30", "Fortaleza"));
 		String result = dataAnalysis.buildFinalString(values);
@@ -37,6 +39,11 @@ public class DataAnalysisTest {
 		String result1 = dataAnalysis.buildFinalString(values1);
 		assertTrue(dataAnalysis.getResultStringNamesAndAge(values1),
 				result1.contains("The first name is greater or equals in size to the last name and the participant older than 30"));
+	
+		values2.addAll(Arrays.asList("Game", "123", "123", "M", "32", "Fortaleza"));
+		String result2 = dataAnalysis.buildFinalString(values2);
+		assertTrue(dataAnalysis.getResultStringNamesAndAge(values2),
+				result2.contains("No analysis was performed"));
 	}
 
 }
