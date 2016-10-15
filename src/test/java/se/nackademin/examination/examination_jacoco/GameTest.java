2e1b5f96a64932dd87f520f82c93218b70bc5d71
Daniel Sjösten <dansjo_87@hotmail.com>
2016-10-15 10:43:25 +0200

    Added tests in the gametest class

--- src/test/java/se/nackademin/examination/examination_jacoco/GameTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/GameTest.java
@@ -17,10 +17,7 @@ public class GameTest {
 		values.addAll(Arrays.asList("Game", "Daniel", "Sjösten", "M", "30", "Stockholm"));
 //		String result = dataAnalysis.buildFinalString(values);
 		game.run(values);
-		//assertEquals(values, result.contains("Game", "Daniel", "Sjösten", "M", "30", "Stockholm")); ??
-
-		
-		
+		//assertEquals(values, result.contains("Game", "Daniel", "Sjösten", "M", "30", "Stockholm")); ??	
 	}
 	
 	
@@ -36,8 +33,75 @@ public class GameTest {
 		
 		i = game.calculateOutPutBasedOnNames("Kalle", "Kalle");
 		assertEquals("The result should be 2", i, 2);
+	}
+	
+	@Test
+	public void testCalculateOutPutBasedOnGender() {
+		Game game = new Game();
+		
+		//Downcasting int to char just to try it out
+		char c = (char) game.calculateOutPutBasedOnGender('M');
+		assertEquals("The result should be 0", c, 0);
+		
+		int i = game.calculateOutPutBasedOnGender('M');
+		assertEquals("The result should be 0", i, 0);
+		
+		c = (char) game.calculateOutPutBasedOnGender('F');
+		assertEquals("The result should be 1", c, 1);
+		
+		c = (char) game.calculateOutPutBasedOnGender('N');
+		assertEquals("The result should be 2", c, 2);
 		
 
+	}
+	
+	@Test
+	public void testCalculateOutPutBasedOnAge() {
+		Game game = new Game();
+		
+		int i = game.calculateOutPutBasedOnAge(25);
+		assertEquals("The result should be 0", i, 0);
+		
+		i = game.calculateOutPutBasedOnAge(26);
+		assertEquals("The result should be 1", i, 1);
+	}
+	
+	@Test
+	public void testCalculateOutPutBasedOnHomeCity() {
+		Game game = new Game();
+		
+		int i = game.calculateOutPutBasedOnHomeCity("Alingsås");
+		assertEquals("The result should be 0", i, 0);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Borås");
+		assertEquals("The result should be 1", i, 1);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Charlottenberg");
+		assertEquals("The result should be 2", i, 2);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Dalby");
+		assertEquals("The result should be 3", i, 3);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Ekerö");
+		assertEquals("The result should be 4", i, 4);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Falun");
+		assertEquals("The result should be 5", i, 5);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Göteborg");
+		assertEquals("The result should be 6", i, 6);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Halmstad");
+		assertEquals("The result should be 7", i, 7);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Ingared");
+		assertEquals("The result should be 8", i, 8);
+		
+		i = game.calculateOutPutBasedOnHomeCity("Jönköping");
+		assertEquals("The result should be 9", i, 9);
+		
+		i = game.calculateOutPutBasedOnHomeCity("1");
+		assertEquals("The result should be 10", i, 10);
 	}
 
 }
